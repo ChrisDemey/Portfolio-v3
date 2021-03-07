3e05bcb4da26e4be9ee1a8dc67cfa9e8bbfde747
ChrisDemey <49678590+ChrisDemey@users.noreply.github.com>
2021-03-07 19:10:00 +0100

    💫 Added some animations and transitions

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,16 @@
   >
     <Container>
       <Navbar />
-      <router-view />
+      <transition
+        enter-active-class="transition duration-300 ease-out"
+        enter-class="opacity-0"
+        enter-to-class="opacity-100"
+        leave-active-class="transition duration-200 ease-in"
+        leave-class="opacity-100"
+        leave-to-class="opacity-0"
+      >
+        <router-view />
+      </transition>
     </Container>
   </div>
 </template>
