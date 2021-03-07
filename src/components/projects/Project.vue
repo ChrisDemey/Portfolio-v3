fedb4625fd4a91a40c9a25bb795dd6e1e24ebc91
ChrisDemey <49678590+ChrisDemey@users.noreply.github.com>
2021-03-07 18:16:08 +0100

    🍱 Updated assets img in projects
    - Added borders of projects item

--- src/components/projects/Project.vue
+++ src/components/projects/Project.vue
@@ -1,14 +1,14 @@
 <template>
   <div class="grid grid-cols-2 gap-4 space-y-0 md:grid-cols-3 md:gap-x-6 md:gap-y-12">
     <ul v-for="item in projects" :key="item.id">
-      <li class="rounded-lg shadow-lg">
+      <li class="p-px bg-white rounded-lg shadow-lg">
         <a class="cursor-pointer aspect-w-3 aspect-h-2" :href="item.link" target="_blank">
-          <img class="object-cover rounded-t-lg shadow" :src="item.image">
+          <img class="object-cover border-none rounded-t-lg shadow" :src="item.image">
         </a>
         <div class="px-2 pt-2 pb-2 space-y-2 bg-white rounded-b-lg md:px-4 md:pb-4">
-          <div class="space-y-1 text-base font-medium leading-6 md:text-lg">
-            <h3>{{ item.name }}</h3>
-          </div>
+          <h3 class="text-base font-medium leading-6 md:text-lg">
+            {{ item.name }}
+          </h3>
           <div class="space-x-2 text-right">
             <Badge
               v-for="(tech, index) in item.techs"
